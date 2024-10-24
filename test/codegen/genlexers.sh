3a684da7e9686ce5b8cab7666beb79f80c204e8a
stefan-hoeck <hock@zhaw.ch>
2024-10-24 18:23:24 +0200

    [ pack ] update code generator

--- test/codegen/genlexers.sh
+++ test/codegen/genlexers.sh
@@ -2,3 +2,4 @@
 
 pack --log-level silence exec src/Gen.idr >src/Lexer.idr
 pack --log-level silence exec src/Package.idr >src/PackageLexer.idr
+pack --log-level silence exec src/Smiles.idr >src/SmilesLexer.idr
